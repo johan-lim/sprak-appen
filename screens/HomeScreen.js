7b7d5007a5cdeef40b1618d4bec090e0098179c2
Johan Lim <jjohanlim@gmail.com>
2020-03-29 08:31:18 +0200

    better JSON mock

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,40 +1,49 @@
 import React, { Component } from 'react';
 import { Image } from 'react-native';
 import { Container, Header, View, DeckSwiper, Card, CardItem, Thumbnail, Text, Left, Body, Icon } from 'native-base';
-const cards = [
-  {
-    text: 'A robot',
-    name: 'En robot',
-    image: require('../assets/images/robot-dev.png'),
-  },
-  {
-    text: 'A star',
-    name: 'En stjärna',
-    image: require('../assets/images/robot-prod.png'),
-  }
-];
+
+const lesson = {
+  name: 'Första lektionen engelska',
+  level: 1,
+  words: [
+    {
+      language_from: 'A robot',
+      language_to: 'En robot',
+      image: require('../assets/images/robot-dev.png'),
+    },
+    {
+      language_from: 'A star',
+      language_to: 'En stjärna',
+      image: require('../assets/images/robot-prod.png'),
+    },
+    {
+      language_from: 'A pen',
+      language_to: 'En penna',
+      image: require('../assets/images/robot-prod.png'),    }
+  ]
+}
+
 export default class HomeScreen extends Component {
   render() {
     return (
       <Container>
-        <Header />
         <View>
           <DeckSwiper
-            dataSource={cards}
+            dataSource={lesson.words}
             renderItem={item =>
               <Card style={{ elevation: 3 }}>
                 <CardItem>
                   <Left>
                     <Thumbnail source={item.image} />
                     <Body>
-                      <Text>{item.text}</Text>
+                      <Text>{item.language_from}</Text>
                       <Text note>Substantiv</Text>
                     </Body>
                   </Left>
                 </CardItem>
                 <CardItem>
                   <Icon name="heart" style={{ color: '#ED4A6A' }} />
-                  <Text>{item.name}</Text>
+                  <Text>{item.language_to}</Text>
                 </CardItem>
               </Card>
             }
