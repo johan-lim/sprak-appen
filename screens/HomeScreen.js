4e636dbac05f51c0482d60da85927373a5d6787b
Johan Lim <jjohanlim@gmail.com>
2020-04-23 11:09:37 +0200

    swipe right for known words

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,44 +1,65 @@
 import React, { Component } from 'react';
-import { Image } from 'react-native';
-import { Container, Header, View, DeckSwiper, Card, CardItem, Thumbnail, Text, Left, Body, Icon } from 'native-base';
+import { Container, Spinner, View, DeckSwiper, Card, CardItem, Thumbnail, Text, Left, Body, Icon } from 'native-base';
 import { mainConfig } from '../utils/config';
 
 export default class HomeScreen extends Component {
   constructor(props) {
     super(props);
-    this.state = { currentLesson: undefined };
+    this.state = { currentWordList: [], isLoading: false, knownWords: [] };
   }
 
   fetchLesson = async (langFrom, langTo, level) => {
-    const response = await fetch(`${mainConfig.storage_url}/lesson_${langFrom}_${langTo}_${level}.json`);
+    this.setState({ isLoading: true });
+    const response = await fetch(`${mainConfig.storage_url}/lesson_${langFrom}_${langTo}_${level}.json?bustcache=true`);
     const currentLesson = await response.json();
-    this.setState({ currentLesson });
+    this.setState({ currentWordList: currentLesson.words, isLoading: false });
   }
 
   componentDidMount() {
     this.fetchLesson('eng', 'swe', 1);
   }
 
+  shuffleArray(array) {
+      for (let i = array.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [array[i], array[j]] = [array[j], array[i]];
+      }
+      return array;
+  }
+
+  knownWord = (item) => {
+    this.setState({ knownWords: [...this.state.knownWords, item], currentWordList: this.state.currentWordList.filter(word => word !== item) });
+  }
+
   render() {
     return (
       <Container>
         <View>
-          {this.state.currentLesson &&
+          <Text>Words I know:</Text>
+          {this.state.knownWords.map((item, i) => <Text key={i}>{item.language_from}</Text>)}
+          {this.state.isLoading && <Spinner />}
+          {this.state.currentWordList.length > 0 &&
             <DeckSwiper
-            dataSource={this.state.currentLesson.words}
+            dataSource={this.shuffleArray(this.state.currentWordList)}
+            onSwipeRight={(item) => this.knownWord(item)}
             renderItem={item =>
               <Card style={{ elevation: 3 }}>
                 <CardItem>
                   <Left>
                     <Body>
                       <Text>{item.language_from}</Text>
-                      <Text note>Substantiv</Text>
+                      <Text note>English</Text>
                     </Body>
                   </Left>
                 </CardItem>
                 <CardItem>
-                  <Icon name="flag" style={{ color: '#ED4A6A' }} />
-                  <Text>{item.language_to}</Text>
+                <Left>
+                    <Body>
+                      <Icon name="flag" style={{ color: '#ED4A6A' }} />
+                      <Text>{item.language_to}</Text>
+                      <Text note>Swedish</Text>
+                    </Body>
+                  </Left>
                 </CardItem>
               </Card>
             }
